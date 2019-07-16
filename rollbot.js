95e09b2e9dafdda229d40c9db0e4df583144a511
Michael Brewer <mb1985@gmail.com>
2019-07-16 12:22:50 -0400

    Add Heroku Procfile

--- rollbot.js
+++ rollbot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const config = require('./config.json');
+//const config = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
